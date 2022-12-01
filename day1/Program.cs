a60ad1736662d0a8b258e720455918cafb3dabe0
pzrwk <p.pozarowszczyk@outlook.com>
2022-12-01 17:09:32 +0100

    Day 1 done

---
+++ day1/Program.cs
@@ -0,0 +1,25 @@
+﻿var text = File.ReadAllLines("input.txt");
+
+List<int> result = new();
+int caloriesCount = 0;
+
+foreach (var line in text)
+{
+    if (line == "")
+    {
+        result.Add(caloriesCount);
+        caloriesCount = 0;
+    }
+    else
+    {
+        caloriesCount += Convert.ToInt32(line);
+    }
+}
+
+var mostCalories = result.Max();
+Console.WriteLine($"Elf number {result.IndexOf(mostCalories)} carrying {mostCalories} calories");
+
+// Part 2
+var caloriesOfTop3 = result.OrderDescending().Take(3).Sum();
+
+Console.WriteLine($"Calories carried by top 3 elves: {caloriesOfTop3}");
